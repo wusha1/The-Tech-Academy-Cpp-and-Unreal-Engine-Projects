e4045020ecf033aff481f62581b2b8298157223e
wusha1 <allcompass11@outlook.com>
2026-07-06 23:48:02 -0700

    Completed calculator tutorial setup and updated console output

--- CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
@@ -1,7 +1,12 @@
-//Calculator app tutorial
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Calculator Console Application" << endl << endl;
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+
+    return 0;
 }
