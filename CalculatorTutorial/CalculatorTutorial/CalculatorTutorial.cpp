03c3b161cfe1c8441e030ee56978c8e0cc40b7bd
wusha1 <allcompass11@outlook.com>
2026-07-09 18:11:45 -0700

    Added divide-by-zero handling and practiced Visual Studio debugging

--- CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
+++ CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
@@ -18,7 +18,15 @@ int main()
     while (true)
     {
         cin >> x >> oper >> y;
-        result = c.Calculate(x, oper, y);
+        if (oper == '/' && y == 0)
+        {
+            cout << "Division by 0 exception" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
         cout << "Result is: " << result << endl;
     }
 
