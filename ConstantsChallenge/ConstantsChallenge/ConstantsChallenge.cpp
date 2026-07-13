9bbc3c2ba44b0545d455a5ac62376fc6fb33d0f5
wusha1 <allcompass11@outlook.com>
2026-07-12 23:09:20 -0700

    Complete Constants Challenge

---
+++ ConstantsChallenge/ConstantsChallenge/ConstantsChallenge.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    // This shows the const variable that cannot be changed anymore after its been created
+    const double PI = 3.14159;
+
+    std::cout << "The value of PI is: " << PI << "\n";
+
+    return 0;
+}
