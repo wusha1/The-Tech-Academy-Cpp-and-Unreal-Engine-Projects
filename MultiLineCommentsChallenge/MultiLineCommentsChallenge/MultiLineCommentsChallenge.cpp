478f4d6db9ef2426a96f986441a5aade7166f6e6
wusha1 <allcompass11@outlook.com>
2026-07-13 00:28:11 -0700

    Complete Multi-Line Comments Challenge

---
+++ MultiLineCommentsChallenge/MultiLineCommentsChallenge/MultiLineCommentsChallenge.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    /*
+        This is a multi-line comment.
+        This is used to wriet notes and explaing parts of code
+    */
+
+    std::cout << "Multi-line comments challenge complete!\n";
+
+    return 0;
+}
