071229a1848999b704b802d22d1f9f88e94a01ea
wusha1 <allcompass11@outlook.com>
2026-07-13 00:55:13 -0700

    Complete For Loop Challenge

---
+++ ForLoopChallenge/ForLoopChallenge/ForLoopChallenge.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    // This program shows a for loop, which is useful when you know how many times you want the loop to run
+
+    for (int count = 1; count <= 5; count++)
+    {
+        std::cout << "Count: " << count << "\n";
+    }
+
+    return 0;
+}
