21971ab8f4aa5b56e9e95c52d5ef367681f2be5a
wusha1 <allcompass11@outlook.com>
2026-07-12 23:57:09 -0700

    Completed Numbers Challenge

---
+++ NumbersChallenge/NumbersChallenge/NumbersChallenge.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    // This introduces the float data type which is used to store decimal numbers also shows 72.5* farenheit display in console
+    float temperature = 72.5f;
+
+    std::cout << "The temperature is: " << temperature << "\n";
+
+    return 0;
+}
