ad203767bf7b4f3b2b6436421e45e4968c610dcc
wusha1 <allcompass11@outlook.com>
2026-07-13 01:01:44 -0700

    Complete Array Challenge

---
+++ ArrayChallenge/ArrayChallenge/ArrayChallenge.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    // This program shows how to declare and use an array
+
+    std::string fruits[] = { "Apple", "Banana", "Orange", "Grapes" };
+
+    std::cout << "The third fruit is: " << fruits[2] << "\n";
+
+    return 0;
+}
