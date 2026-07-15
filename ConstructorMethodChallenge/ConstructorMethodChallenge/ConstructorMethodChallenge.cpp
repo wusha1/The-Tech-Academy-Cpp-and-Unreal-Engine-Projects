811f105e1b6fda4907026d3639b7c007f865280e
wusha1 <allcompass11@outlook.com>
2026-07-14 20:12:15 -0700

    Complete Constructor Method Challenge

--- ConstructorMethodChallenge/ConstructorMethodChallenge/ConstructorMethodChallenge.cpp
+++ ConstructorMethodChallenge/ConstructorMethodChallenge/ConstructorMethodChallenge.cpp
@@ -1,20 +1,29 @@
-// ConstructorMethodChallenge.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+class Student
+{
+public:
+    std::string name;
+    int age;
+
+    // Construct declaration
+    Student(std::string studentName, int studentAge);
+};
+
+    // Constructor is outside the class
+Student::Student(std::string studentName, int studentAge)
+{
+    name = studentName;
+    age = studentAge;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Student student("Victor", 25);
+
+    std::cout << "Name: " << student.name << "\n";
+    std::cout << "Age: " << student.age << "\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
