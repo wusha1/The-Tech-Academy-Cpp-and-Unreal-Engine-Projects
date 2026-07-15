b69422d2877a2328e8d019d160f331464697c00b
wusha1 <allcompass11@outlook.com>
2026-07-14 22:28:14 -0700

    Breed: Hound Color: Brown Height: 2 feet Weight: 60 pounds  The dog shakes. The dog sits. The dog lays down.

---
+++ OOPSubmissionAssignment/OOPSubmissionAssignment/OOPSubmissionAssignment.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+class Dog
+{
+public:
+    // Properties / State
+    std::string breed;
+    std::string color;
+    std::string height;
+    std::string weight;
+
+    // Methods / Behavior
+    void shake()
+    {
+        std::cout << "The dog shakes.\n";
+    }
+
+    void sit()
+    {
+        std::cout << "The dog sits.\n";
+    }
+
+    void layDown()
+    {
+        std::cout << "The dog lays down.\n";
+    }
+};
+
+int main()
+{
+    // Creates an object from the Dog class
+    Dog dog;
+
+    // Gives the object its state
+    dog.breed = "Hound";
+    dog.color = "Brown";
+    dog.height = "2 feet";
+    dog.weight = "60 pounds";
+
+    // Displays the object's properties
+    std::cout << "Breed: " << dog.breed << "\n";
+    std::cout << "Color: " << dog.color << "\n";
+    std::cout << "Height: " << dog.height << "\n";
+    std::cout << "Weight: " << dog.weight << "\n\n";
+
+    // Calls the object's methods
+    dog.shake();
+    dog.sit();
+    dog.layDown();
+
+    return 0;
+}
